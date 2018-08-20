40bdd0e4b4b7913628303b6a7cc7dc242bf2ac4e
unknown <almeshari93@gmail.com>
2018-08-20 22:15:04 +0300

    20-8-2018 10:14 PM

--- src/types/graphql-utils.ts
+++ src/types/graphql-utils.ts
@@ -1,10 +1,15 @@
 import { Redis } from "ioredis";
+export interface Session {
+    userId?: string;
+};
 
 export interface ResolverMap  {
     [Key: string]: {
         [Key: string]: (parent: any, args: any, context: {
             redis: Redis;
             url: string;    
+            session: Session;
         }, info: any) => any;
-    }
+    };
 }
+
