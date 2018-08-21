13126d55a22d12aa927c5e2eee41786a174023ce
unknown <almeshari93@gmail.com>
2018-08-21 21:38:21 +0300

    21-8-2018 = 9:38 pm

--- src/types/graphql-utils.ts
+++ src/types/graphql-utils.ts
@@ -1,15 +1,30 @@
 import { Redis } from "ioredis";
-export interface Session {
+ export interface Session {
     userId?: string;
 };
+export type Resolver = ( parent: any, args: any, context: {
+    redis: Redis;
+    url: string;    
+    session: Session;
+}, info: any) => any;
+
+export type GraphQLMiddlewareFunc = (
+    resolver: Resolver,
+    parent: any,
+    args: any, 
+    context: {
+        redis: Redis;
+        url: string;    
+        session: Session;
+    },
+     info: any
+    ) => any;
+
+
 
 export interface ResolverMap  {
     [Key: string]: {
-        [Key: string]: (parent: any, args: any, context: {
-            redis: Redis;
-            url: string;    
-            session: Session;
-        }, info: any) => any;
+        [Key: string]: Resolver; 
     };
 }
 
